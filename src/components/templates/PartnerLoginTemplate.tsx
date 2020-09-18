d72c5807440f08912ffe9752152f38abfc07f12d
大西雄貴 <yuki.onishi1129@gmail.com>
2020-09-19 04:04:06 +0900

    パートナーログインページ完成

---
+++ src/components/templates/PartnerLoginTemplate.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useState } from 'react'
+import AuthForm from '@/components/organisms/common/AuthForm'
+import LinkButtonField from '@/components/molcules/LinkButtonField'
+import LoginForm from '@/components/molcules/LoginFrom'
+import useAuth from '@/contexts/auth'
+import { LinkStatus } from '@/utils/consts'
+import { EventType } from '@/types/events'
+import { RequestLogin } from '@/types/api/'
+
+const PartnerLoginTemplate: FC = () => {
+  const { partnerLogin } = useAuth()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleChangeEmail: EventType['onChange'] = (event) => {
+    setEmail(event.target.value)
+  }
+  const handleChangePassword: EventType['onChange'] = (event) => {
+    setPassword(event.target.value)
+  }
+  const handleSubmitLogin = async () => {
+    const requestParam: RequestLogin = {
+      email: email,
+      password: password,
+    }
+    await partnerLogin(requestParam)
+  }
+  return (
+    <AuthForm>
+      <LinkButtonField status={LinkStatus.SIGNUP} />
+      <LoginForm
+        status={LinkStatus.PARTNER_LOGIN}
+        email={email}
+        password={password}
+        changeEmail={handleChangeEmail}
+        changePassword={handleChangePassword}
+        submit={handleSubmitLogin}
+      />
+      <LinkButtonField status={LinkStatus.LOGIN} />
+    </AuthForm>
+  )
+}
+
+export default PartnerLoginTemplate
