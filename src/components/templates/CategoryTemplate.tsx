4e3682bcfd3e9736b0a39421f95d39c5fd33e401
大西雄貴 <yuki.onishi1129@gmail.com>
2020-09-25 01:26:59 +0900

    カテゴリ管理のヘッダー部分作成

--- src/components/templates/CategoryTemplate.tsx
+++ src/components/templates/CategoryTemplate.tsx
@@ -7,10 +7,29 @@ const CategoryTemplate: FC = () => {
   return (
     <CategoryProvider>
       <ContentsMain>
-        <div>aaa</div>
+        <TitleHeader>
+          <DetailLinkArea></DetailLinkArea>
+          <h2>カテゴリ管理</h2>
+        </TitleHeader>
       </ContentsMain>
     </CategoryProvider>
   )
 }
 
 export default CategoryTemplate
+
+const TitleHeader = styled.div`
+  display: flex;
+  height: 80px;
+  line-height: 80px;
+  font-size: 1.75rem;
+  font-weight: bold;
+  color: #d163a2;
+  text-align: center;
+`
+
+const DetailLinkArea = styled.div`
+  /* border: 1px solid red; */
+  width: 480px;
+  height: 80px;
+`
