bbcd17b599f1643a004039d6399cb02ef91967ee
大西雄貴 <yuki.onishi1129@gmail.com>
2020-09-28 19:01:24 +0900

    Layout.tsx削除したので微調整

--- src/pages/calendar/[date].tsx
+++ src/pages/calendar/[date].tsx
@@ -2,7 +2,6 @@ import React, { FC, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { ProtectRoute } from '@/contexts/auth'
-import Layout from '@/components/Layout'
 import styled from 'styled-components'
 import ApiClient from '@/network/ApiClient'
 import { LinkStatus, BeforeLoginPage, AfterLoginPage } from '@/utils/consts'
@@ -13,22 +12,20 @@ const Calender: FC = () => {
   useEffect(() => {}, [])
   return (
     <div>
-      <Layout>
-        <H1>カレンダー</H1>
-        <p>{router.query.date}</p>
-        <div onClick={() => router.push(AfterLoginPage.GRAPH)}>グラフ</div>
-        <div>
-          <Link
-            href={`${AfterLoginPage.DETAIL}[date]`}
-            as={`${AfterLoginPage.DETAIL}${CurrentDate()}`}
-          >
-            日別
-          </Link>
-        </div>
-        <div onClick={() => router.push(AfterLoginPage.PARTNER_USER)}>
-          パートナー
-        </div>
-      </Layout>
+      <H1>カレンダー</H1>
+      <p>{router.query.date}</p>
+      <div onClick={() => router.push(AfterLoginPage.GRAPH)}>グラフ</div>
+      <div>
+        <Link
+          href={`${AfterLoginPage.DETAIL}[date]`}
+          as={`${AfterLoginPage.DETAIL}${CurrentDate()}`}
+        >
+          日別
+        </Link>
+      </div>
+      <div onClick={() => router.push(AfterLoginPage.PARTNER_USER)}>
+        パートナー
+      </div>
     </div>
   )
 }
