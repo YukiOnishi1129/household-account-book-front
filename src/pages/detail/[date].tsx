2670aab204fb6d13d2b7c2cdfdf658bab0333f1c
大西雄貴 <yuki.onishi1129@gmail.com>
2020-10-25 23:38:31 +0900

    不要な資材を削除

--- src/pages/detail/[date].tsx
+++ src/pages/detail/[date].tsx
@@ -1,12 +1,6 @@
-import React, { FC, useEffect } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React, { FC } from 'react'
 import { ProtectRoute } from '@/contexts/auth'
 import DetailTemplate from '@/components/templates/DetailTemplate'
-import styled from 'styled-components'
-import ApiClient from '@/network/ApiClient'
-import { LinkStatus, BeforeLoginPage, AfterLoginPage } from '@/utils/consts'
-import { CurrentDate } from '@/utils/date'
 
 const Detail: FC = () => {
   return <DetailTemplate />
